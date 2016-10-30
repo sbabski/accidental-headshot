787599acf0e549bfe0d3d8822bfe4b918be97518
Sarah Babski <babski.s@324RY-D8.local>
2016-10-30 14:29:28 -0400

    testing index template and mongo

--- index.py
+++ index.py
@@ -1,9 +1,33 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect
+import os
+from pymongo import MongoClient
 
-@app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+def connect():
+	connection = MongoClient('ds053176.mlab.com', 53176)
+	handle = connection['sarah']
+	handle.authenticate('sarah', 'bei.pfi')
+	return handle
+
+
+app = Flask(__name__)
+handle = connect()
+
+
+@app.route('/', methods=['GET'])
+def index():
+	userinputs = [x for x in handle.mycollection.find()]
+	return render_template('index.html', userinputs=userinputs)
+
+@app.route('/write', methods=['POST'])
+def write():
+	userinput = request.form.get('userinput')
+	oid = handle.mycollection.insert({'message':userinput})
+	return redirect ('/')
+
+@app.route('/deleteall', methods=['GET'])
+def deleteall():
+	handle.mycollection.remove()
+	return redirect ('/')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
